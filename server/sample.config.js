2fa06bbf099a3dfea7fb6cca9ce8023aac1150f5
Sahil Kumar <sahilkumar142002@gmail.com>
2024-01-31 10:55:06 +0530

    created sample config file for future refrence.

---
+++ server/sample.config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET: "",
+    EMAIL_USERNAME:"",
+    EMAIL_PASSWORD:"",
+    ATLAS_URI:'mongodb+srv://<username>:<password>@cluster0.juyqf.mongodb.net/<database_name>?retryWrites=true&w=majority'
+}
