3568d26c714e27b6702fc3e24bbc5df89825fce5
Sahil Kumar <sahilkumar142002@gmail.com>
2024-02-05 10:35:23 +0530

    Auth Modified

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,8 +4,7 @@ import ENV from '../config.js'
 export default async function Auth(req,res,next) {
     try {
         // access authorize header to validate request
-        const token = req.headers.authorization;
-
+        const token = req.headers.authorization.split(' ')[1];
         // retrive the user details fo the logged in user
         const decodedToken = await jwt.verify(token, ENV.JWT_SECRET);
 
