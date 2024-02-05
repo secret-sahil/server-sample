c4aaaad92109f2dec2db2e42fccd2f296d05777a
Sahil Kumar <sahilkumar142002@gmail.com>
2024-02-05 14:22:33 +0530

    fixed a little bug

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -12,7 +12,7 @@ export default function Products() {
 	}, [])
 
 	return (
-		<div className="relative overflow-x-auto">
+		<div className={getProducts ? "hidden" : "relative overflow-x-auto"}>
 			<table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
 				<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
 					<tr>
