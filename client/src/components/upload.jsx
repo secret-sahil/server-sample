e307becc44a2959eb5bae8f2324aab395d57c76a
Sahil Kumar <sahilkumar142002@gmail.com>
2024-02-05 13:05:55 +0530

    added upload button

--- client/src/components/upload.jsx
+++ client/src/components/upload.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { UploadToDB } from '../helper/helper'
-
+import toast, { Toaster } from 'react-hot-toast'
 export default function Upload() {
 	let [getFile, setFile] = useState(false)
 
@@ -19,25 +19,34 @@ export default function Upload() {
 	}
 
 	function handleUpload(data) {
-        console.log('working');
+		console.log('working')
 		let File = data.target.files[0]
 		if (isXlsx(File.name)) {
-            setFile(File);
-        } else{
-            setFile(false)
-        }
+			setFile(File)
+		} else {
+			setFile(false)
+		}
 	}
 
-    function uploadFile() {
-        UploadToDB(getFile)
-    }
+	function uploadFile() {
+        const formData = new FormData();
+        formData.append('file', getFile);
+        
+		let uploadPromise = UploadToDB(formData)
+		toast.promise(uploadPromise, {
+			loading: 'Uploading...',
+			success: <b>Uploaded Successfully...!</b>,
+			error: <b>Could not Uploaded!</b>,
+		})
+	}
 
-    function removeFile() {
-        setFile(false)
-    }
+	function removeFile() {
+		setFile(false)
+	}
 
 	return (
 		<div className="h-screen font-sans text-gray-900 bg-gray-300 border-box">
+			<Toaster position="top-right" reverseOrder={false}></Toaster>
 			<div className="flex justify-center w-full mx-auto sm:max-w-lg">
 				<div className="flex flex-col items-center justify-center w-full h-auto my-20 bg-white sm:w-3/4 sm:rounded-lg sm:shadow-xl">
 					<div className="mt-10 mb-10 text-center">
@@ -63,7 +72,9 @@ export default function Upload() {
 							className="z-20 flex flex-col-reverse items-center justify-center w-full h-full cursor-pointer"
 						>
 							<p className="z-10 text-xs font-light text-center text-gray-500">
-								{getFile ? getFile.name : 'Drag & Drop your files here'}
+								{getFile
+									? getFile.name
+									: 'Drag & Drop your files here'}
 							</p>
 							<svg
 								className="z-10 w-8 h-8 text-indigo-400"
@@ -74,14 +85,26 @@ export default function Upload() {
 								<path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" />
 							</svg>
 						</label>
-                            <div className={getFile ? 'flex justify-evenly' : 'hidden'}>
-                                <button onClick={uploadFile} type='button' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-[45%] mt-20">
-                                    Upload
-                                </button>
-                                <button onClick={removeFile} type='button' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-[45%] mt-20">
-                                    Remove File
-                                </button>
-                            </div>
+						<div
+							className={
+								getFile ? 'flex justify-evenly' : 'hidden'
+							}
+						>
+							<button
+								onClick={uploadFile}
+								type="button"
+								className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-[45%] mt-20"
+							>
+								Upload
+							</button>
+							<button
+								onClick={removeFile}
+								type="button"
+								className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-[45%] mt-20"
+							>
+								Remove File
+							</button>
+						</div>
 					</form>
 				</div>
 			</div>
--- client/src/helper/helper.jsx
+++ client/src/helper/helper.jsx
@@ -1,16 +1,20 @@
-import axios from 'axios';
+import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.REACT_APP_SERVER_DOMAIN;
-/** Make API Requests */    
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_DOMAIN
+/** Make API Requests */
 
 /** Upload function */
-export async function UploadToDB({ file }){
-    try {
-        if(file){
-            const { data } = await axios.post('/api/upload', { file })
-            return Promise.resolve({ data });
-        }
-    } catch (error) {
-        return Promise.reject({ error : "Invalid file type."})
-    }
+export async function UploadToDB(file) {
+	try {
+		if (file) {
+			const { data } = await axios.post('/api/upload', file, {
+				headers: {
+					'Content-Type': 'multipart/form-data',
+				},
+			})
+			return Promise.resolve({ data })
+		}
+	} catch (error) {
+		return Promise.reject({ error: 'Invalid file type.' })
+	}
 }
